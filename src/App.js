a2bff19d9dd23f9741e7b507c15b7249386902a6
Denis <wolf-den@tut.by>
2021-08-11 08:44:23 +0300

    add state likes for increment+decrement and value for input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
+
+  const increment = () => {
+    setLikes(likes + 1);
+  };
+  const decrement = () => {
+    setLikes(likes - 1);
+  };
+
   return (
     <div className="App">
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type="text" value={value} onChange={event => setValue(event.target.value)} />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }

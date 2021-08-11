09ab4efc1704bd190eedaf2b9497e1ef224687dd
Denis <wolf-den@tut.by>
2021-08-11 08:59:16 +0300

    add function class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
+import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 
 function App() {
-  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
 
   return (
     <div className="App">
-      <Counter />
+      <ClassCounter />
     </div>
   );
 }

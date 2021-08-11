5e5244eda421c739e8a6e7addda95a02196ba30e
Denis <wolf-den@tut.by>
2021-08-11 09:29:03 +0300

    mapping data in postList

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
-import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 import './styles/App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript 1', body: 'Description 1' },
+    { id: 2, title: 'JavaScript 2', body: 'Description 2' },
+    { id: 3, title: 'JavaScript 3', body: 'Description 3' },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python 1', body: 'Description 1' },
+    { id: 2, title: 'Python 2', body: 'Description 2' },
+    { id: 3, title: 'Python 3', body: 'Description 3' },
+  ]);
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: 'JavaScript', body: 'Description' }} />
-      <PostItem post={{ id: 2, title: 'JavaScript', body: 'Description' }} />
-      <PostItem post={{ id: 3, title: 'JavaScript', body: 'Description' }} />
-      <PostItem post={{ id: 4, title: 'JavaScript', body: 'Description' }} />
-      <PostItem post={{ id: 5, title: 'JavaScript', body: 'Description' }} />
+      <PostList posts={posts} title={'Посты про JS'} />
+      <PostList posts={posts2} title={'Посты про Python'} />
     </div>
   );
 }

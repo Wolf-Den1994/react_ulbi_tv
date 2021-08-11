c1ca1010964a575a0886add11be1b482410fda44
Denis <wolf-den@tut.by>
2021-08-11 09:15:41 +0300

    add props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import PostItem from './components/PostItem';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. JavaScript</strong>
-          <div>
-            JavaScript - язык программирования
-          </div>
-        </div>
-        <div className="post__btns">\
-          <button>Удалить</button>
-        </div>
-      </div>
+      <PostItem post={{ id: 1, title: 'JavaScript', body: 'Description' }} />
+      <PostItem post={{ id: 2, title: 'JavaScript', body: 'Description' }} />
+      <PostItem post={{ id: 3, title: 'JavaScript', body: 'Description' }} />
+      <PostItem post={{ id: 4, title: 'JavaScript', body: 'Description' }} />
+      <PostItem post={{ id: 5, title: 'JavaScript', body: 'Description' }} />
     </div>
   );
 }
